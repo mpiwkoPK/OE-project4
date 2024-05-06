00ef1ceaa7fbcb11e17d27c52e0eafd44c89546e
mpiwkoPK <magdalena.piwko@student.pk.edu.pl>
2024-05-06 20:43:23 +0200

    chyba alfa i beta hihi

--- Helpers/crossingMethods.py
+++ Helpers/crossingMethods.py
@@ -61,19 +61,34 @@ class LinearCrossover(CrossoverMethod):
 
 
 class BlendCrossoverAlfaBeta(CrossoverMethod):
-    def __init__(self, alpha):
-        self.alpha = alpha
+    def __init__(self, probability):
+        self.probability = probability
 
     def crossover(self, pop: np.ndarray, alpha=0.2):
         n_pop, n_dim = pop.shape
         parents = pop[np.random.choice(n_pop, 2, replace=False)]
-        gen_idx = np.random.randint(0, n_dim)
-        p1_gen = parents[0, gen_idx]
-        p2_gen = parents[1, gen_idx]
-        child1 = parents[0]
-        child2 = parents[1]
-        child1[gen_idx] = (1 - alpha) * p1_gen + alpha * p2_gen
-        child2[gen_idx] = (1 - alpha) * p2_gen + alpha * p1_gen
+        print("RODZIC: ", parents)
+        child1 = np.empty_like(parents[0])
+        child2 = np.empty_like(parents[0]) 
+    
+        if np.random.uniform() < self.probability:
+            alpha = np.random.uniform(low=0, high=1)
+            beta = np.random.uniform(low=0, high=1)
+            
+
+            for i in range(len(parents[0])):
+                x1 = parents[0][i]
+                x2 = parents[1][i]
+                distancia = abs(x1-x2)
+
+                u1 = np.random.uniform(min(x1,x2)-alpha*distancia, max(x1,x2)+beta*distancia)
+                u2 = np.random.uniform(min(x1,x2)-alpha*distancia, max(x1,x2)+beta*distancia)
+
+                child1[i] = u1
+                child2[i] = u2
+
+        print("DZIECKO1: ", child1)
+        print("DZIECKO2: ", child2)
 
         return np.array([child1, child2])
 
